ddd52e078b16b5ca026c810762518cf012943cff
sadigzadeamina07 <Sadigzadeamina7@gmail.com>
2026-04-01 16:45:11 +0400

    created Hero section

---
+++ src/Component/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { SlBasket } from "react-icons/sl";
+import { CiHeart } from "react-icons/ci";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+function Header() {
+    return (
+        <div className='bg-[#ecfdff]'>
+
+            <div className='max-w-[1200px] mx-auto py-5'>
+                <div className="flex justify-between   items-center">
+                    <img src="/logo.png"  className="object-cover" alt="" />
+                    <ul className='flex gap-3 text-lg'> 
+                        <li>Home</li>
+                        <li>
+                            Shop
+                        </li>
+                        <li>Pages
+                        </li>
+                        <li>Blog
+                        </li>
+                        <li>Contact</li>
+                    </ul>
+                    <div className="flex gap-3  text-xl">
+                        <HiMagnifyingGlass />
+                        <CiHeart />
+                                <SlBasket />
+                                
+                    </div>
+            
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Header
